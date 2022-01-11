4e1c64b3db1f8585de78a583f865193ad2f8bba2
Priyanka Maheshwari <priyankamaheshwari.2021@gmail.com>
2022-01-11 21:40:26 +0100

    Error handling at one place

---
+++ utils/async-handler.js
@@ -0,0 +1,11 @@
+const asyncHandler = (callback) => {
+  return async (req, res, next) => {
+    try {
+      return await callback(req, res, next);
+    } catch (err) {
+      console.error(err);
+      res.status(404).render('./error_handlers/404');
+    }
+  };
+};
+export default asyncHandler;
