74bef8ff4824725f6db2951fab3ce10105221995
Priyanka Maheshwari <priyankamaheshwari.2021@gmail.com>
2022-01-11 21:38:45 +0100

    Intializing client

---
+++ prismic-configuration.js
@@ -0,0 +1,28 @@
+// node-fetch is used to make network requests to the Prismic Rest API.
+// In Node.js Prismic projects, you must provide a fetch method to the
+// Prismic client.
+import fetch from 'node-fetch';
+import * as prismic from '@prismicio/client';
+
+export const repoName = 'sample-site'; // Fill in your repository name.
+const endpoint = prismic.getEndpoint(repoName); // Format your endpoint.
+
+export const client = prismic.createClient(endpoint, {
+  // If your repo is private, add an access token.
+  accessToken: '',
+  fetch,
+
+  // This defines how you will structure URL paths in your project.
+  // Update the types to match the Custom Types in your project, and edit
+  // the paths to match the routing in your project.
+  routes: [
+    {
+      type: 'homepage',
+      path: '/',
+    },
+    {
+      type: 'page',
+      path: '/:uid',
+    },
+  ],
+});
